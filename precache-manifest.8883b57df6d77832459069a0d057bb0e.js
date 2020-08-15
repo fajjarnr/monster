4792b0de11ea6dc0ae82c0e0101c765918875b48
fajjarnr <zfajart@gmail.com>
2020-08-15 19:16:32 +0700

    Updates

---
+++ precache-manifest.8883b57df6d77832459069a0d057bb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b374120f7ed1028c8a80a397a08b98fb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "86402c7ad2f9bf2eb419",
+    "url": "/monster-rolodex/static/css/main.65e2a836.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monster-rolodex/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86402c7ad2f9bf2eb419",
+    "url": "/monster-rolodex/static/js/main.5c72fe85.chunk.js"
+  },
+  {
+    "revision": "fa483a6908201fa18e92",
+    "url": "/monster-rolodex/static/js/runtime-main.3adb4eac.js"
+  }
+]);
