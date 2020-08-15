77c78a3608ac173f558dc2cb2497f94e5ef8cb33
fajjarnr <zfajart@gmail.com>
2020-08-15 10:09:20 +0700

    first commit

---
+++ src/component/CardList/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+import { Card } from "../Card";
+
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
